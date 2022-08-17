b0ffd4c40c45fc7d7bcbbca4cf9d7d4dfe90800a
youngjin-no <85482428+youngjin-no@users.noreply.github.com>
2022-08-17 18:00:50 +0900

    feat(board) : package 구조 추가

---
+++ board/src/main/java/com/study/board/domain/board/controller/BoardController.java
@@ -0,0 +1,10 @@
+package com.study.board.domain.board.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+}
---
+++ board/src/main/java/com/study/board/domain/board/service/BoardService.java
@@ -0,0 +1,10 @@
+package com.study.board.domain.board.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+}
