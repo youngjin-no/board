395a5e15e5260458d6ca62d75b7db5475f45ac8e
kjy <junyeobk@naver.com>
2022-09-03 13:41:37 +0900

    업데이트

--- board/src/main/java/com/study/board/domain/board/controller/BoardViewController.java
+++ board/src/main/java/com/study/board/domain/board/controller/BoardViewController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.study.board.domain.board.entity.Board;
@@ -19,10 +20,22 @@ public class BoardViewController {
 
 	private final BoardService boardService;
 
+	/*
+	* 게시판 리스트 화면
+	* */
 	@GetMapping("/list")
 	public String boardList(Model model) {
 		List<Board> boards = boardService.findBoards();
 		model.addAttribute("boards", boards);
 		return "board/list";
 	}
+
+	/*
+	* 게시판 등록 화면
+	* */
+	@GetMapping("/write")
+	public String boardWrite(){
+		return "board/write";
+	}
+
 }
